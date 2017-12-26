1bdaab19f34a5a6c7941a9ba051f1ad635c13a93
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2017-12-26 17:16:10 +0700

    add network id and rpcdomain

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@ if [ ! -d /build/vet/geth/chaindata ]
 then
   wallet=$(geth account new --password /build/.pwd --datadir /build/vet | awk -v FS="({|})" '{print $2}')
   geth --datadir /build/vet init /build/genesis.json
-  geth --bootnodes enode://bbfa82ac7012a8ba5fa99b0c127446e8876ce9e1d15c641c462356eacc1921da049033809c07e2eb6518bf6d57070a303afc843fcc3ee22516bdbb38270081a3@52.76.54.184:30303 --datadir /build/vet --rpc --rpcaddr 0.0.0.0 --rpcport 8545 --unlock "${wallet}" --password /build/.pwd --mine
+  geth --bootnodes enode://bbfa82ac7012a8ba5fa99b0c127446e8876ce9e1d15c641c462356eacc1921da049033809c07e2eb6518bf6d57070a303afc843fcc3ee22516bdbb38270081a3@52.76.54.184:30303 --datadir /build/vet --networkid 40686 --rpc --rpccorsdomain "*" --rpcaddr 0.0.0.0 --rpcport 8545 --unlock "${wallet}" --password /build/.pwd --mine
 else
   wallet=$(geth account list --datadir /build/vet| head -n 1 | awk -v FS="({|})" '{print $2}')
-  geth --bootnodes enode://bbfa82ac7012a8ba5fa99b0c127446e8876ce9e1d15c641c462356eacc1921da049033809c07e2eb6518bf6d57070a303afc843fcc3ee22516bdbb38270081a3@52.76.54.184:30303 --datadir /build/vet --rpc --rpcaddr 0.0.0.0 --rpcport 8545 --unlock "${wallet}" --password /build/.pwd --mine
+  geth --bootnodes enode://bbfa82ac7012a8ba5fa99b0c127446e8876ce9e1d15c641c462356eacc1921da049033809c07e2eb6518bf6d57070a303afc843fcc3ee22516bdbb38270081a3@52.76.54.184:30303 --datadir /build/vet --networkid 40686 --rpc --rpccorsdomain "*" --rpcaddr 0.0.0.0 --rpcport 8545 --unlock "${wallet}" --password /build/.pwd --mine
 fi
