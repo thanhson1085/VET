c041f56dfedc9b8ae7c4192d38c0ff2c776fe4a5
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2018-01-23 13:52:50 +0700

    set gaslimit, expert 2000 trans per second

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,8 +17,8 @@ if [ ! -d /build/vet/geth/chaindata ]
 then
   wallet=$(geth account new --password /build/.pwd --datadir /build/vet | awk -v FS="({|})" '{print $2}')
   geth --datadir /build/vet init /build/genesis.json
-  geth --bootnodes ${bootnodes} --datadir /build/vet --networkid 40686 --rpc --rpccorsdomain "*" --rpcaddr 0.0.0.0 --rpcport 8545 --unlock "${wallet}" --password /build/.pwd --mine --gasprice "100000000"
+  geth --bootnodes ${bootnodes} --datadir /build/vet --networkid 40686 --rpc --rpccorsdomain "*" --rpcaddr 0.0.0.0 --rpcport 8545 --unlock "${wallet}" --password /build/.pwd --mine --gasprice "100000000" --targetgaslimit "42000000"
 else
   wallet=$(geth account list --datadir /build/vet| head -n 1 | awk -v FS="({|})" '{print $2}')
-  geth --bootnodes ${bootnodes} --datadir /build/vet --networkid 40686 --rpc --rpccorsdomain "*" --rpcaddr 0.0.0.0 --rpcport 8545 --unlock "${wallet}" --password /build/.pwd --mine --gasprice "100000000"
+  geth --bootnodes ${bootnodes} --datadir /build/vet --networkid 40686 --rpc --rpccorsdomain "*" --rpcaddr 0.0.0.0 --rpcport 8545 --unlock "${wallet}" --password /build/.pwd --mine --gasprice "100000000" --targetgaslimit "42000000"
 fi
